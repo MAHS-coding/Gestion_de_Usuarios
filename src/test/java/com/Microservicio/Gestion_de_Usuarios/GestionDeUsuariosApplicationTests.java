262c71ff0a17bd81f2d0264f498c30e63f1423cf
Matias <mahscoding@gmail.com>
2025-05-16 11:40:16 -0400

    first commit

---
+++ src/test/java/com/Microservicio/Gestion_de_Usuarios/GestionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservicio.Gestion_de_Usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
